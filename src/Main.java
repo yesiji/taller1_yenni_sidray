ff4b1f40e0f835fc661073dd88d2f0046f35a584
sidray <yennisidray@gmail.com>
2023-11-04 23:17:39 -0500

    Mi primer proyecto en Java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int altura = 10;
+        int base = 6;
+
+        int area = (altura * base) / 2;
+
+            System.out.println("El área del triángulo con base " + base + " y con altura " + altura + " es: " + area);
+    }
+}
